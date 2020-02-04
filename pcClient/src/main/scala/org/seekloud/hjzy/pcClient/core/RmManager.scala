bfd9e8136e8c8991caad5d0b625a9177681901d9
zhangwanqing0930 <zhangwanqing@ebupe.com>
2020-02-04 14:27:47 +0800

    1

--- pcClient/src/main/scala/org/seekloud/hjzy/pcClient/core/RmManager.scala
+++ pcClient/src/main/scala/org/seekloud/hjzy/pcClient/core/RmManager.scala
@@ -20,9 +20,18 @@ object RmManager {
 
   private val log = LoggerFactory.getLogger(this.getClass)
 
+  var userInfo: Option[UserInfo] = None
+  var roomInfo: Option[RoomInfo] = None
+
   sealed trait RmCommand
 
   final case class GetHomeItems(homeScene: HomeScene, homeController: HomeController) extends RmCommand
+
+  final case object GoToLive extends RmCommand
+
+  final case object GoToRoomHall extends RmCommand
+
+  final case object Logout extends RmCommand
 
   final case object StopSelf extends RmCommand
 
@@ -51,6 +60,7 @@ object RmManager {
   private def idle(
     stageCtx: StageContext,
     mediaPlayer: MediaPlayer,
+    homeController: Option[HomeController] = None,
   )(
     implicit stashBuffer: StashBuffer[RmCommand],
     timer: TimerScheduler[RmCommand]
@@ -59,12 +69,48 @@ object RmManager {
       case msg: GetHomeItems =>
         idle(stageCtx, mediaPlayer)
 
-
-
-
       case StopSelf =>
         log.info(s"rmManager stopped in idle.")
         Behaviors.stopped
+
+//      case GoToLive =>
+//        val hostScene = new HostScene(stageCtx.getStage)
+//        val hostController = new HostController(stageCtx, hostScene, ctx.self)
+//
+//        def callBack(): Unit = Boot.addToPlatform(hostScene.changeToggleAction())
+//
+//        liveManager ! LiveManager.DevicesOn(hostScene.gc, callBackFunc = Some(callBack))
+//        ctx.self ! HostWsEstablish
+//        Boot.addToPlatform {
+//          if (homeController != null) {
+//            homeController.get.removeLoading()
+//          }
+//          hostController.showScene()
+//        }
+//        switchBehavior(ctx, "hostBehavior", hostBehavior(stageCtx, homeController, hostScene, hostController, liveManager, mediaPlayer))
+//
+//      case GoToRoomHall =>
+//        val roomScene = new RoomScene()
+//        val roomController = new RoomController(stageCtx, roomScene, ctx.self)
+//        Boot.addToPlatform {
+//          if (homeController != null) {
+//            homeController.get.removeLoading()
+//          }
+//          roomController.showScene()
+//        }
+//        idle(stageCtx, liveManager, mediaPlayer, homeController, Some(roomController))
+
+
+
+
+
+
+      case Logout =>
+        log.info(s"logout.")
+        this.roomInfo = None
+        this.userInfo = None
+        homeController.get.showScene()
+        Behaviors.same
 
       case x =>
         log.warn(s"unknown msg in idle: $x")
--- pcClient/src/main/scala/org/seekloud/hjzy/pcClient/scene/HomeScene.scala
+++ pcClient/src/main/scala/org/seekloud/hjzy/pcClient/scene/HomeScene.scala
@@ -1,7 +1,9 @@
 package org.seekloud.hjzy.pcClient.scene
 
+import javafx.geometry.Pos
 import javafx.scene.control.Button
 import javafx.scene.image.ImageView
+import javafx.scene.layout.{BorderPane, HBox, VBox}
 import javafx.scene.{Group, Scene}
 import org.seekloud.hjzy.pcClient.common.Constants._
 import org.slf4j.LoggerFactory
@@ -15,6 +17,23 @@ import org.slf4j.LoggerFactory
 object HomeScene{
 
   trait HomeSceneListener{
+
+    def gotoLogin(
+      userName: Option[String] = None,
+      pwd: Option[String] = None,
+      isToLive: Boolean = false,
+      isToWatch: Boolean = false
+    )
+
+    def gotoRegister()
+
+    def logout()
+
+    def gotoRoomHall()
+
+    def gotoLive()
+
+    def editInfo()
 
   }
 
@@ -45,6 +64,13 @@ class HomeScene {
     this.listener = listener
   }
 
+  /*waitingGif*/
+  val waitingGif = new ImageView("img/gif/waiting.gif")
+  waitingGif.setFitHeight(50)
+  waitingGif.setFitWidth(50)
+  waitingGif.setLayoutX(width / 2 - 25)
+  waitingGif.setLayoutY(height / 2 - 25)
+
   /*background*/
   val background = new ImageView("img/picture/background.jpg")
   background.setFitHeight(height)
@@ -52,16 +78,32 @@ class HomeScene {
 
   /*topArea*/
   val loginBtn = new Button("登录")
+  loginBtn.setOnAction(_ => listener.gotoLogin())
   val registerBtn = new Button("注册")
-
+  registerBtn.setOnAction(_ => listener.gotoRegister())
+  val topBox = new HBox(20, loginBtn, registerBtn)
+  topBox.setPrefSize(width, height*0.2)
+  topBox.setAlignment(Pos.CENTER)
 
 
 
 
   /*middleArea*/
+  val joinBtn = new Button("参加会议")
+  joinBtn.setOnAction(_ => listener.gotoRoomPage())
+  val createBtn = new Button("创建会议")
+  createBtn.setOnAction(_ => listener.liveCheck())
+  val middleBox = new VBox(30, joinBtn, createBtn)
+  middleBox.setPrefSize(width, height*0.6)
+  middleBox.setAlignment(Pos.CENTER)
 
 
+  /*layout*/
+  val borderPane = new BorderPane()
+  borderPane.setTop(topBox)
+  borderPane.setCenter(middleBox)
 
-  group.getChildren.addAll(background)
+
+  group.getChildren.addAll(background, borderPane)
 
 }
