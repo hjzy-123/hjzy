63a120b7db9bba826ed5fe771165a8be3529da70
zhangwanqing0930 <zhangwanqing@ebupe.com>
2020-02-05 23:14:33 +0800

    1

---
+++ pcClient/src/main/scala/org/seekloud/hjzy/pcClient/scene/MeetingScene.scala
@@ -0,0 +1,50 @@
+package org.seekloud.hjzy.pcClient.scene
+
+import javafx.scene.{Group, Scene}
+import org.seekloud.hjzy.pcClient.common.Constants.AppWindow
+import org.slf4j.LoggerFactory
+
+/**
+  * Author: zwq
+  * Date: 2020/2/5
+  * Time: 22:45
+  */
+object MeetingScene {
+  trait MeetingSceneListener{
+
+
+
+  }
+
+}
+class MeetingScene{
+  import MeetingScene._
+
+  private[this] val log = LoggerFactory.getLogger(this.getClass)
+
+  private val width = AppWindow.width * 0.9
+  private val height = AppWindow.height * 0.75
+
+  val group = new Group()
+
+  private val scene = new Scene(group, width, height)
+  scene.getStylesheets.add(
+    this.getClass.getClassLoader.getResource("css/meetingSceneCss.css").toExternalForm
+  )
+
+  def getScene: Scene = {
+    this.scene
+  }
+
+  var listener: MeetingSceneListener = _
+
+  def setListener(listener: MeetingSceneListener): Unit = {
+    this.listener = listener
+  }
+
+
+
+  group.getChildren.addAll()
+
+
+}
